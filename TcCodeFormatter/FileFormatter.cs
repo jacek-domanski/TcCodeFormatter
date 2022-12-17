e3ed03009f5bf1a20085819add4fcee9d5d92bd1
jacek-domanski <j.domanski92@gmail.com>
2022-12-17 14:40:26 +0100

    Initial commit

---
+++ TcCodeFormatter/FileFormatter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace TcCodeFormatter
+{
+	class FileFormatter
+	{
+		private XmlDocument document = new XmlDocument();
+		List<XmlNode> declarations = new List<XmlNode>();
+		List<XmlNode> implementations = new List<XmlNode>();
+
+		public FileFormatter(string filePath)
+		{
+			document.Load(filePath);
+		}
+
+		public void formatCode()
+		{
+			addDeclarationsAndImplementations(this.document.DocumentElement);
+
+			Console.WriteLine("Declarations count: " + this.declarations.Count);
+
+			foreach (XmlNode declaration in this.declarations)
+			{
+				string[] lines = splitNodeTextIntoLines(declaration);
+
+				foreach (string line in lines)
+				{
+					Console.WriteLine(line);
+				}
+
+				Console.WriteLine("============================================");
+			}
+
+			Console.WriteLine("Implementations count: " + this.implementations.Count);
+
+			foreach (XmlNode implementation in this.implementations)
+			{
+				string[] lines = splitNodeTextIntoLines(implementation);
+				foreach (string line in lines)
+				{
+					Console.WriteLine(line);
+				}
+
+				Console.WriteLine("============================================");
+			}
+		}
+
+		private void addDeclarationsAndImplementations(XmlNode node)
+		{
+			string declarationName = "Declaration";
+			string implementationName = "Implementation";
+
+			if (node.Name == declarationName)
+			{
+				this.declarations.Add(node);
+			} else if (node.Name == implementationName && node.FirstChild.Name == "ST")
+			{
+				this.implementations.Add(node.FirstChild);
+			}
+
+			if (node.HasChildNodes)
+			{
+				foreach(XmlNode child in node.ChildNodes)
+				{
+					addDeclarationsAndImplementations(child);
+				}
+			}
+		}
+
+		private string[] splitNodeTextIntoLines(XmlNode node)
+		{
+			return node.InnerText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+		}
+	}
+}
---
+++ TcCodeFormatter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace TcCodeFormatter
+{
+	class Program
+	{
+		static List<XmlNode> declarations;
+		static List<XmlNode> implementations;
+		static void Main(string[] args)
+		{
+			string filePath = new string("C:\\Projects\\CSharp\\TcCodeFormatter\\FB_StringBuilder.TcPOU");
+
+			FileFormatter fileFormatter = new FileFormatter(filePath);
+
+			fileFormatter.formatCode();
+
+
+		}
+	}
+}
