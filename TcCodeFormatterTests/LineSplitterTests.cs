b1f75f3ff5ebc61bc865659fea9cc086adbd7fff
jacek-domanski <j.domanski92@gmail.com>
2022-12-21 18:20:40 +0100

    Add test check whether splitting code with special segment in the middle works correctly

--- TcCodeFormatterTests/LineSplitterTests.cs
+++ TcCodeFormatterTests/LineSplitterTests.cs
@@ -96,7 +96,38 @@ namespace TcCodeFormatterTests
 			Assert.AreEqual("iInt := 5; (* comment *) {pragma} // endline", list2[0].Text, "code 2 differs from input");
 			Assert.AreEqual("iInt := 5; (* comment *) 'string' // endline", list3[0].Text, "code 3 differs from input");
 			Assert.AreEqual("iInt := 5; (* comment *) 'string' {pragma}", list4[0].Text, "code 4 differs from input");
+		}
+		[TestMethod]
+		public void Should_returnListWithThreeElements_When_splitCalledOnCodeWithSpecialSegmentInside()
+		{
+			// Arrange
+			string code1 = "sString := 'text';";
+			string code2 = "iInt := (* comment *) 5;";
+			LineSplitter lineSplitter = new LineSplitter();
 
+			// Act
+			List<CodeLineSegment> list1 = lineSplitter.split(code1);
+			List<CodeLineSegment> list2 = lineSplitter.split(code2);
+
+			// Assert
+			Assert.AreEqual(3, list1.Count, "list 1 is not length 3");
+			Assert.AreEqual(3, list2.Count, "list 2 is not length 3");
+
+			Assert.AreEqual(SegmentType.Code, list1[0].SegmentType, "code 1 segment 0 type is not code");
+			Assert.AreEqual(SegmentType.StringLiteral, list1[1].SegmentType, "code 1 segment 1 type is not StringLiteral");
+			Assert.AreEqual(SegmentType.Code, list1[2].SegmentType, "code 1 segment 2 type is not code");
+
+			Assert.AreEqual(SegmentType.Code, list2[0].SegmentType, "code 2 segment type is not code");
+			Assert.AreEqual(SegmentType.MultilineComment, list2[1].SegmentType, "code 2 segment type is not multiline comment");
+			Assert.AreEqual(SegmentType.Code, list2[2].SegmentType, "code 2 segment type is not code");
+
+			Assert.AreEqual("sString := ", list1[0].Text, "code 1 segment 0 differs from input");
+			Assert.AreEqual("text", list1[1].Text, "code 1 segment 1 differs from input");
+			Assert.AreEqual(";", list1[2].Text, "code 1 segment 2 differs from input");
+
+			Assert.AreEqual("iInt := ", list2[0].Text, "code 2 segment 0 differs from input");
+			Assert.AreEqual(" comment ", list2[1].Text, "code 2 segment 1 differs from input");
+			Assert.AreEqual(" 5;", list2[2].Text, "code 2 segment 2 differs from input");
 		}
 	}
 }
