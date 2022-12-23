b6ed4a18fec649addeccacbb0f05bc7c675714dd
jacek-domanski <j.domanski92@gmail.com>
2022-12-23 18:26:41 +0100

    Add empty LineBuilder class

---
+++ TcCodeFormatter/LineBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcCodeFormatter
+{
+	public class LineBuilder
+	{
+		public LineBuilder()
+		{
+		}
+
+		public void reset()
+		{
+
+		}
+
+		public void append(CodeLineSegment segment)
+		{
+
+		}
+
+		public string getLine()
+		{
+			return "";
+		}
+	}
+}
