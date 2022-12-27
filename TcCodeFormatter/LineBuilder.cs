546a7bef8d84ee96f770d49c10d590f289a61c75
jacek-domanski <j.domanski92@gmail.com>
2022-12-27 15:58:20 +0100

    Line builder - implement append method

--- TcCodeFormatter/LineBuilder.cs
+++ TcCodeFormatter/LineBuilder.cs
@@ -21,12 +21,132 @@ namespace TcCodeFormatter
 
 		public void append(CodeLineSegment segment)
 		{
+			switch (segment.SegmentType)
+			{
+				case SegmentType.Unkown:
+					appendUnknown(segment);
+					break;
+				case SegmentType.Code:
+					appendCode(segment);
+					break;
+				case SegmentType.MultilineComment:
+					appendMultilineComment(segment);
+					break;
+				case SegmentType.EndlineComment:
+					appendEndlineComment(segment);
+					break;
+				case SegmentType.StringLiteralSingleQuote:
+					appendStringLiteralSingleQuote(segment);
 
+					break;
+				case SegmentType.StringLiteralDoubleQuote:
+					appendStringLiteralDoubleQuote(segment);
+
+					break;
+				case SegmentType.Pragma:
+					appendPragma(segment);
+
+					break;
+				default:
+					throw new NotImplementedException("Appending " + segment.SegmentType.ToString() + " segment is not implemented");
+			}
 		}
 
 		public string getLine()
 		{
 			return this.line;
+		}
+
+		private void appendUnknown(CodeLineSegment segment)
+		{
+			throw new ArgumentException("Cannot append unknown type segment");
+		}
+
+		private void appendCode(CodeLineSegment segment)
+		{
+			appendSpaceIfNoWhitespaceAtTheEnd();
+			this.line += segment.Text;
+		}
+
+		private void appendMultilineComment(CodeLineSegment segment)
+		{
+			if (segment.HasStartMarker)
+			{
+				appendSpaceIfNoWhitespaceAtTheEnd();
+				line += "(* ";
+			}
+
+			this.line += segment.Text;
+
+			if (segment.HasEndMarker)
+			{
+				line += " *)";
+			}
+		}
+
+		private void appendEndlineComment(CodeLineSegment segment)
+		{
+			appendSpaceIfNoWhitespaceAtTheEnd();
+			this.line += "// ";
+			this.line += segment.Text;
+		}
+
+		private void appendStringLiteralSingleQuote(CodeLineSegment segment)
+		{
+			if (segment.HasStartMarker)
+			{
+				appendSpaceIfNoWhitespaceAtTheEnd();
+				line += "'";
+			}
+
+			this.line += segment.Text;
+
+			if (segment.HasEndMarker)
+			{
+				line += "'";
+			}
+		}
+
+		private void appendStringLiteralDoubleQuote(CodeLineSegment segment)
+		{
+			if (segment.HasStartMarker)
+			{
+				appendSpaceIfNoWhitespaceAtTheEnd();
+				line += "\"";
+			}
+
+			this.line += segment.Text;
+
+			if (segment.HasEndMarker)
+			{
+				line += "\"";
+			}
+		}
+
+		private void appendPragma(CodeLineSegment segment)
+		{
+			if (segment.HasStartMarker)
+			{
+				appendSpaceIfNoWhitespaceAtTheEnd();
+				line += "{";
+			}
+
+			this.line += segment.Text;
+
+			if (segment.HasEndMarker)
+			{
+				line += "}";
+			}
+		}
+
+		private void appendSpaceIfNoWhitespaceAtTheEnd()
+		{
+			bool appendSpace =
+				!this.line.EndsWith(" ")
+				&& !this.line.EndsWith("\t")
+				&& this.line.Length > 0;
+
+			if (appendSpace) this.line += " ";
 		}
 	}
 }
