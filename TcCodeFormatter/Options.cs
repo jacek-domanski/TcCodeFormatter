4300789d4731852484656a1a734358e44a03d1c9
jacek-domanski <j.domanski92@gmail.com>
2022-12-30 21:01:59 +0100

    Add command line argument parser

---
+++ TcCodeFormatter/Options.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcCodeFormatter
+{
+	internal class Options
+	{
+		[Option('v', "verbose", Required = false, HelpText = "TcCodeFormatter becomes talkative")]
+		public bool Verbose { get; set; }
+
+		[Option('f', "files", Required = false, HelpText = "Input files to be formatted, separated by spaces. If full path is not given, current directory will be searched. File extension is not necessary")]
+		public IEnumerable<string> InputFiles { get; set; }
+
+		[Option('a', "all", Required = false, HelpText = "Searches current directory recursively, formats all known files found")]
+		public bool All { get; set; }
+
+		[Option('d', "diff", Required = false, HelpText = "Formats only files found by git diff --stat. Requires git installed")]
+		public bool Diff { get; set; }
+	}
+}
--- TcCodeFormatter/Program.cs
+++ TcCodeFormatter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
+using CommandLine;
 
 namespace TcCodeFormatter
 {
@@ -8,6 +10,10 @@ namespace TcCodeFormatter
 	{
 		static void Main(string[] args)
 		{
+			CommandLine.Parser.Default.ParseArguments<Options>(args)
+				.WithParsed(runOptions)
+				.WithNotParsed(handleParseError);
+
 			string[] filesPaths = new[] { "C:\\Projects\\CSharp\\TcCodeFormatter\\POUs\\FB_StringBuilder.TcPOU" };
 
 			FileFormatter fileFormatter = null;
@@ -18,6 +24,14 @@ namespace TcCodeFormatter
 				fileFormatter.formatCode();
 				fileFormatter.save(filePath + "2");
 			}
+		}
+		static void runOptions(Options options)
+		{
+			Console.WriteLine("Files: " + options.InputFiles.Count() + ", All: " + options.All + ", Diff: " + options.Diff);
+		}
+		static void handleParseError(IEnumerable<Error> errs)
+		{
+			Console.WriteLine("Parsing error");
 		}
 	}
 }
