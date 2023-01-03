5f3f157a14a01f036a9fdf882d73c2b9fd9c8f5f
jacek-domanski <j.domanski92@gmail.com>
2023-01-03 19:40:32 +0100

    Formatter now removes a line, if more than one consecutive line is empty or whitespace only\

--- TcCodeFormatter/Formatters/Formatter.cs
+++ TcCodeFormatter/Formatters/Formatter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
+using System.Text.RegularExpressions;
+using TcCodeFormatter.Utilities;
 
 namespace TcCodeFormatter
 {
@@ -10,6 +12,8 @@ namespace TcCodeFormatter
 		private const string ENDLINE = "\r\n";
 		private LineSplitter lineSplitter;
 		private LineBuilder lineBuilder;
+
+		private bool lastLineWasEmpty;
 
 		public Formatter()
 		{
@@ -40,7 +44,17 @@ namespace TcCodeFormatter
 		}
 		private void oldLineToNew(string oldLine, List<string> newLines)
 		{
-			List <CodeLineSegment> segments = this.lineSplitter.split(oldLine);
+			List<CodeLineSegment> segments = this.lineSplitter.split(oldLine);
+
+			if (isThisLineEmpty(segments))
+			{
+				if (lastLineWasEmpty) return;
+				lastLineWasEmpty = true;
+			}
+			else
+			{
+				lastLineWasEmpty = false;
+			}
 
 			segments
 				.FindAll(x => x.SegmentType == SegmentType.Code)
@@ -51,6 +65,18 @@ namespace TcCodeFormatter
 
 			newLines.Add(this.lineBuilder.getLine());
 		}
+
+		private static bool isThisLineEmpty(List<CodeLineSegment> segments)
+		{
+			bool textIsEmptyOrWhitespace = 
+				segments[0].Text == "" || Regexes.whitespaceOnly.IsMatch(segments[0].Text);
+
+			return 
+				segments.Count == 1 
+				&& segments[0].SegmentType == SegmentType.Code 
+				&& textIsEmptyOrWhitespace;
+		}
+
 		private void formatCode(CodeLineSegment codeSegment)
 		{
 			if (codeSegment.SegmentType != SegmentType.Code)
